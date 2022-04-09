25ab56627954159d324906804290ec84bafccfd5
aybergoguz <103028734+aybergoguz@users.noreply.github.com>
2022-04-09 16:43:16 +0300

    Ucuncu asama tamamlandi

--- odev.py
+++ odev.py
@@ -1,6 +1,8 @@
 def toplama(no1,no2):
     return no1+no2
 def bolme(no1,no2):
+    if(no2==0):
+        return 0
     return no1/no2
 print("Toplama Sonucu="+str(toplama(10,20)))
 print("Bolme Sonucu="+str(bolme(100,20)))
